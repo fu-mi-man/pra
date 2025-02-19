17b4ea4598f543cd0937b5ec76291b28b0364db3
fu-mi-man <fumichan0393@gmail.com>
2025-02-19 21:08:27 +0900

    モデルを実装

--- backend/project/app/Models/Category.php
+++ backend/project/app/Models/Category.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    // テーブル名の明示的な指定
+    protected $table = 'categories';
+
+    // 一括代入可能な属性の指定
+    protected $fillable = [
+        'name',
+        'type',
+        'enterprise_id',
+        'display_order',
+    ];
+
+    // 型キャスト
+    protected $casts = [
+        'enterprise_id' => 'integer',
+        'display_order' => 'integer',
+    ];
 }
