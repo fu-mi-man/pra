2cf5476036ec355e03aa96cd26a84f2b2010b673
fu-mi-man <fumichan0393@gmail.com>
2025-06-07 23:35:42 +0900

    feat: 絞り込み機能をなんとなく実装

---
+++ frontend/project/pages/drawer/index.vue
@@ -0,0 +1,106 @@
+<template>
+  <v-container>
+    <v-btn
+      class="mb-4"
+      icon
+      @click="drawer = true"
+    >
+      <v-icon>mdi-filter-variant</v-icon>
+    </v-btn>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      :right="right"
+      app
+      temporary
+      :width="400"
+    >
+      <v-list>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title class="title">
+              絞り込み
+            </v-list-item-title>
+          </v-list-item-content>
+          <v-list-item-action>
+            <v-btn
+              icon
+              @click="drawer = false"
+            >
+              <v-icon>mdi-close</v-icon>
+            </v-btn>
+          </v-list-item-action>
+        </v-list-item>
+
+        <v-divider />
+
+        <!-- 展開可能なパネル -->
+        <v-expansion-panels>
+          <!-- 商品種別 -->
+          <v-expansion-panel>
+            <v-expansion-panel-header>
+              商品種別
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              <v-select
+                v-model="selectedProductType"
+                :items="productTypes"
+                label="商品種別"
+                outlined
+                dense
+                hide-details
+                class="mt-2"
+              ></v-select>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+
+          <!-- 公開状態 -->
+          <v-expansion-panel>
+            <v-expansion-panel-header>
+              公開状態
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              <v-select
+                v-model="selectedPublishStatus"
+                :items="publishStatuses"
+                label="公開状態"
+                outlined
+                dense
+                hide-details
+                class="mt-2"
+              ></v-select>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+
+        </v-expansion-panels>
+      </v-list>
+    </v-navigation-drawer>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      right: false,
+      // 商品種別の選択肢
+      productTypes: [
+        { text: '全て', value: 'all' },
+        { text: '登録した商品', value: 'own' },
+        { text: '共有された商品', value: 'shared' }
+      ],
+      selectedProductType: 'all', // 選択された商品種別
+      // 公開状態の選択肢
+      publishStatuses: [
+        { text: '全て', value: 'all' },
+        { text: '未公開', value: 'private' },
+        { text: '一般公開', value: 'public' },
+        { text: '顧客限定公開', value: 'customer' },
+        { text: 'グループ限定公開', value: 'group' }
+      ],
+      selectedPublishStatus: 'all', // 選択された公開状態
+    }
+  }
+}
+</script>
