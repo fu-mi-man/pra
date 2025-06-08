31ff61eb6088679c80b8d85d08a19ff73dbc1d1d
fu-mi-man <fumichan0393@gmail.com>
2025-06-08 18:16:45 +0900

    feat: スクロールできるように微調整など

--- frontend/project/pages/drawer/index.vue
+++ frontend/project/pages/drawer/index.vue
@@ -14,9 +14,10 @@
       app
       temporary
       :width="400"
+      class="d-flex flex-column"
     >
       <!-- ヘッダー部分 -->
-      <div class="px-4">
+      <div class="px-4 drawer__header">
         <div class="d-flex py-3 align-center justify-space-between">
           <div class="text-h6">絞り込み</div>
           <v-btn
@@ -31,7 +32,7 @@
       <v-divider />
 
       <!-- 条件部分 -->
-      <v-list class="pa-0 drawer-content">
+      <v-list class="pa-0 flex-grow-1 overflow-y-auto drawer__content">
         <v-expansion-panels multiple accordion>
           <!-- 商品種別 -->
           <v-expansion-panel>
@@ -71,8 +72,10 @@
         </v-expansion-panels>
       </v-list>
 
+      <v-divider />
+
       <!-- 絞り込みボタン -->
-      <div class="filter-button-container">
+      <div class="pa-4 drawer__footer">
         <v-btn
           block
           color="primary"
@@ -81,7 +84,6 @@
           絞り込む
         </v-btn>
       </div>
-
     </v-navigation-drawer>
   </v-container>
 </template>
@@ -96,7 +98,7 @@ export default {
       productTypes: [
         { text: '全て', value: 'all' },
         { text: '登録した商品', value: 'own' },
-        { text: '共有された商品', value: 'shared' }
+        { text: '共有された商品', value: 'shared' },
       ],
       selectedProductType: 'all', // 選択された商品種別
       // 公開状態の選択肢
@@ -105,7 +107,7 @@ export default {
         { text: '未公開', value: 'private' },
         { text: '一般公開', value: 'public' },
         { text: '顧客限定公開', value: 'customer' },
-        { text: 'グループ限定公開', value: 'group' }
+        { text: 'グループ限定公開', value: 'group' },
       ],
       selectedPublishStatus: 'all', // 選択された公開状態
     }
@@ -116,24 +118,26 @@ export default {
       console.log('商品種別:', this.selectedProductType)
       console.log('公開状態:', this.selectedPublishStatus)
       // ここに実際の絞り込み処理を追加
-    }
+    },
   },
 }
 </script>
 <style scoped>
-.drawer-content {
-  height: calc(100vh - 125px); /* ヘッダーとボタンの高さを考慮 */
+.drawer__header {
+  position: sticky;
+  top: 0;
+  z-index: 2; /* 1だとスクロールが上に来るので2を指定 */
+}
+
+.drawer__content {
+  flex-grow: 1;
+  height: calc(100vh - 130px);
   overflow-y: auto;
 }
 
-.filter-button-container {
-  background-color: white;
-  border-top: 1px solid rgba(0, 0, 0, 0.12);
+.drawer__footer {
   bottom: 0;
-  left: 0;
-  padding: 16px;
-  position: fixed;
-  right: 0;
+  position: sticky;
   z-index: 1;
 }
 </style>
